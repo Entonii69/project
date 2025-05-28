b78a3a49bf30cff935cac0f3707529c677c9444a
Алёхин Антон <entonii@yandex.ru>
2025-05-28 19:43:00 +0300

    Удаление директории project, исправление функции filter_by_state

--- src/processing.py
+++ src/processing.py
@@ -1,20 +1,12 @@
 from datetime import datetime
 
-operations = [
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-]
-
 
 def filter_by_state(operations: list[dict], state: str = 'EXECUTED') -> list[dict]:
     """Функция создает новый список по значению ключа 'state'"""
     new_operations = []
     for operation in operations:
-        for value_state in operation.values():
-            if value_state == state:
-                new_operations.append(operation)
+        if operation['state'] == state:
+            new_operations.append(operation)
     return new_operations
 
 
